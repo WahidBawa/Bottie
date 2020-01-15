3ead8cb4547fd9fdd0e518b225162e2c1d1516de
Wahid Bawa <wahidbawa10@gmail.com>
2020-01-15 13:25:35 -0500

    gitignore

---
+++ bot.py
@@ -0,0 +1,77 @@
+import discord
+from discord.ext import commands
+
+import os
+from dotenv import load_dotenv
+import random
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+client = commands.Bot(command_prefix='!')
+
+
+@client.event
+async def on_ready():
+    print("bot is ready af")
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Latency: {round(client.latency * 1000)}ms")
+    print(str(ctx.guild.members()))
+
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ["It is certain.",
+                 "It is decidedly so.",
+                 "Without a doubt.",
+                 "Yes - definitely.",
+                 "You may rely on it.",
+                 "As I see it, yes.",
+                 "Most likely.",
+                 "Outlook good.",
+                 "Yes.",
+                 "Signs point to yes.",
+                 "Reply hazy, try again.",
+                 "Ask again later.",
+                 "Better not tell you now.",
+                 "Cannot predict now.",
+                 "Concentrate and ask again.",
+                 "Don't count on it.",
+                 "My reply is no.",
+                 "My sources say no.",
+                 "Outlook not so good.",
+                 "Very doubtful."]
+
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+
+
+@client.command()
+async def listMembers(ctx):
+    users = ctx.guild.members()
+    print(users)
+
+
+@client.event
+async def on_message(message):
+    print(message.author.id)
+    print(str(message).strip('\'').split("=")[:])
+
+
+# @client.event
+# async def on_member_join(member):
+#   print(f'{member} has joined a server.')
+
+# @client.event
+# async def on_member_remove(member):
+#   print(f'{member} has left the server.')
+
+
+client.run(token)
